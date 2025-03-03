450df898ed02fecdd9cbd24f67b64a33f2a96769
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-03-03 16:32:46 +0900

    feat: add api_gateway_server2

--- src/main/java/org/example/devopsmain/common/config/CorsConfig.java
+++ src/main/java/org/example/devopsmain/common/config/CorsConfig.java
@@ -17,13 +17,16 @@ public class CorsConfig {
     @Value("${urls.api_gateway_server}")
     private String apiGatewayServerURL;
 
+    @Value("${urls.api_gateway_server2}")
+    private String apiGatewayServer2URL;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                        .allowedOrigins(domainServerURL, apiGatewayServerURL) // 허용할 도메인
+                        .allowedOrigins(domainServerURL, apiGatewayServerURL, apiGatewayServer2URL) // 허용할 도메인
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                         .allowedHeaders("*") // 모든 헤더 허용
                         .allowCredentials(true); // 쿠키 허용 (필요할 경우)
