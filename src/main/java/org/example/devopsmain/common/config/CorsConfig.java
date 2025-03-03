94b1e8170685944f5820ed680fdbb0d060165003
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-03-03 17:31:19 +0900

    feat: change Cors Configuration

--- src/main/java/org/example/devopsmain/common/config/CorsConfig.java
+++ src/main/java/org/example/devopsmain/common/config/CorsConfig.java
@@ -1,15 +1,12 @@
 package org.example.devopsmain.common.config;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class CorsConfig {
     @Value("${urls.domain_server}")
     private String domainServerURL;
@@ -25,11 +22,15 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                        .allowedOrigins(domainServerURL, apiGatewayServerURL, apiGatewayServer2URL) // 허용할 도메인
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                        .allowedHeaders("*") // 모든 헤더 허용
-                        .allowCredentials(true); // 쿠키 허용 (필요할 경우)
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                domainServerURL,
+                                apiGatewayServerURL,
+                                apiGatewayServer2URL
+                        ) // 허용할 도메인
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // 인증 정보 허용 (Authorization, Cookie 등)
             }
         };
     }
