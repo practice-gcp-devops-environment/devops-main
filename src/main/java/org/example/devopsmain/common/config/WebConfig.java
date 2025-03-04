ce4cdf5c5df313a5ba016e173f5f7e2b07ae1d93
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-03-04 13:19:14 +0900

    feat: add Response Logging Interceptor

--- src/main/java/org/example/devopsmain/common/config/WebConfig.java
+++ src/main/java/org/example/devopsmain/common/config/WebConfig.java
@@ -1,6 +1,8 @@
 package org.example.devopsmain.common.config;
 
+import lombok.RequiredArgsConstructor;
 import org.example.devopsmain.common.interceptor.RequestLoggingInterceptor;
+import org.example.devopsmain.common.interceptor.ResponseLoggingInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,13 +11,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final RequestLoggingInterceptor requestLoggingInterceptor;
+    private final ResponseLoggingInterceptor responseLoggingInterceptor;
 
-    public WebConfig(RequestLoggingInterceptor requestLoggingInterceptor) {
+    public WebConfig(RequestLoggingInterceptor requestLoggingInterceptor, ResponseLoggingInterceptor responseLoggingInterceptor) {
         this.requestLoggingInterceptor = requestLoggingInterceptor;
+        this.responseLoggingInterceptor = responseLoggingInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(requestLoggingInterceptor);
+        registry.addInterceptor(responseLoggingInterceptor);
     }
 }
---
+++ src/main/java/org/example/devopsmain/common/interceptor/ResponseLoggingInterceptor.java
@@ -0,0 +1,34 @@
+package org.example.devopsmain.common.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Collection;
+
+@Component
+public class ResponseLoggingInterceptor implements HandlerInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(ResponseLoggingInterceptor.class);
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        // 응답 URI
+        String responseUri = request.getRequestURL().toString();
+        String method = request.getMethod();
+
+        // 모든 응답 헤더 가져오기
+        Collection<String> headerNames = response.getHeaderNames();
+        StringBuilder headerLog = new StringBuilder();
+        for (String headerName : headerNames) {
+            headerLog.append(headerName).append(": ").append(response.getHeader(headerName)).append("\n");
+        }
+
+        logger.info("[OUTBOUND RESPONSE] {} {}", method, responseUri);
+        logger.info("[HEADERS] \n{}", headerLog);
+        logger.info("[STATUS] {}", response.getStatus());
+    }
+}
+
